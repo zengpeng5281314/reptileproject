1a982bbbf79adb1de7f98c92913e058d9c5ec6cf
zengpeng5281314 <zengpeng5281314@163.com>
2019-06-16 23:09:10 +0800

    0.

--- src/main/java/com/mytest/admin/po/TChannleXZDetailedInfoPo.java
+++ src/main/java/com/mytest/admin/po/TChannleXZDetailedInfoPo.java
@@ -3,19 +3,12 @@ package com.mytest.admin.po;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 
 import com.base.annotation.BeanMeta;
 import com.base.common.MBeanBase;
--- src/main/java/com/mytest/admin/service/DownLoadDetailedService.java
+++ src/main/java/com/mytest/admin/service/DownLoadDetailedService.java
@@ -3,6 +3,7 @@ package com.mytest.admin.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.base.common.MParam;
 import com.base.dao.MBeanDAO;
 import com.base.service.MFrameworkService;
 import com.mytest.admin.po.TChannleXZDetailedInfoPo;
@@ -16,13 +17,27 @@ public class DownLoadDetailedService {
 	@Autowired
 	MFrameworkService mFrameworkService;
 	
+	public TChannleXZInfoPo getTChannleXZtailedInfoPo(long userId){
+		MParam mparam = new MParam();
+		mparam.add("userId", userId);
+		return mFrameworkService.get(TChannleXZInfoPo.class, mparam);
+	}
+	
 	public void saveOrUpdateTChannleXZtailedInfoPo(TChannleXZInfoPo tChannleXZtailedInfoPo){
 		mBeanDAO.saveOrUpdate(tChannleXZtailedInfoPo);
+	}
+	
+	public TChannleXZDetailedInfoPo getTChannleXZDetailedInfoPo(long userId,int nameNo){
+		MParam mparam = new MParam();
+		mparam.add("userId", userId);
+		mparam.add("nameNo", nameNo);
+		return mFrameworkService.get(TChannleXZDetailedInfoPo.class, mparam);
 	}
 	
 	public void saveOrUpdateTChannleXZtailedInfoPo(TChannleXZDetailedInfoPo tChannleXZInfoPo){
 		mBeanDAO.saveOrUpdate(tChannleXZInfoPo);
 	}
+	
 	
 	
 	
